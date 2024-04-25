3dfb5adba6dc0ad0cc8c06716db90e207cab87f2
Ajay Sankar J <152076918+Ajay-SJ@users.noreply.github.com>
2024-04-25 16:28:38 +0530

    final project

---
+++ Final_Project_Online_Cake_Shop/ecommerce/store/urls.py
@@ -0,0 +1,19 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #Store Main Page
+
+
+
+    path('',views.store, name='store'),
+
+    #Individual Product
+    
+    path('product/<slug:product_slug>/', views.product_info, name='product-info'),
+    
+    #Individual Category
+
+    path('search/<slug:category_slug>/', views.list_category, name='list-category'),
+]
